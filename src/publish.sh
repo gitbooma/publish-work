42ebbd92f71ad5d2dff70b05ea53de2196c7d2db
gitbooma <96261187+gitbooma@users.noreply.github.com>
2022-09-22 09:55:17 +0200

    Update publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -1,19 +1,22 @@
-VERSION="$1"
-OVERRIDE="$2"
+OVERRIDE="$1"
+IMAGE_NAME="$2"
+TAG_INFO="$3"
 
-echo "VERSION=$VERSION"
+echo "IMAGE_NAME=$IMAGE_NAME"
 echo "OVERRIDE=$OVERRIDE"
 
-VERSION=$VERSION docker-compose -f $OVERRIDE up --no-start --remove-orphans
+docker-compose -f $OVERRIDE up --no-start --remove-orphans
 IMAGES=$(docker inspect --format='{{.Image}}' $(docker ps -aq))
 
 echo "IMAGES: $IMAGES"
 for IMAGE in $IMAGES; do
     echo "IMAGE: $IMAGE"
     
-    NAME=$(basename ${DOCKERHUB_USERNAME}).$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
-    TAG="$DOCKERHUB_USERNAME/$NAME:$VERSION"
+    #NAME=$(basename).$(docker inspect --format '{{ index .Config.Labels "name" }}' $IMAGE)
+    #NAME=${{ env.IMAGE_NAME }}
+    NAME=$IMAGE_NAME
+    NEW_TAG="$DOCKERHUB_USERNAME/$NAME:$TAG_INFO"
 
-    docker tag $IMAGE $TAG
+    docker tag $IMAGE $NEW_TAG
     docker push $TAG
 done
